301e83fdd864957b3ecee157fdc2f89ba860de9f
victorllunes <victorllunes@gmail.com>
2026-03-29 19:37:17 -0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  readonly title = 'AI - Angular - Spring';
+}
